39e16c5b455eaeb12e2e91cbef15fc7930dc1dbf
shafi <mdshafiuddin935@gmail.com>
2024-06-16 13:11:39 +0530

    create test api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config()
-mongoose.connect(process.env.MONGO)
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import userRoutser from "./routes/user.route.js";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
     console.error(err);
@@ -13,10 +15,8 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
 app.listen(port, () => {
   console.log(`the Server is running at http://localhost:${port}`);
 });
+app.use("/api/user", userRoutser);
